2a506fb962186fa5f4b9ffee09a42c78ceb0c9d5
Yoseph <yosephten@gmail.com>
2024-01-17 19:04:13 +0300

    `feat`: prepare controllers

---
+++ src/controllers.ts
@@ -0,0 +1,11 @@
+export function newMessageCountController (req:any, res:any) {
+
+}
+
+export function allMessageController (req:any, res:any) {
+
+}
+
+export function allChatMessageController (req:any, res:any) {
+
+}
