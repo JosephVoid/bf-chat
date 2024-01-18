9e05d3c411093b2d85376bb3610d9609bb738dea
Yoseph <yosephten@gmail.com>
2024-01-18 12:39:52 +0300

    `feat`: added helper functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,12 @@
+import mysql from 'mysql2';
+
+export const conn = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME
+});
+
 export function getRoomId (userId: number, activeUsers:{roomId: string, userId: number}[]): string {
     if (userId <= 0) return '';
     let usr = activeUsers.filter(usr => usr.userId === userId)[0];
