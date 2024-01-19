b85d46b8921d44e702fd42a65e46ce24cf94dec7
Yoseph <yosephten@gmail.com>
2024-01-19 21:31:33 +0300

    `feat`: added API controllers

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,50 +1,108 @@
-import { RowDataPacket } from "mysql2";
-import { conn } from "./helpers";
+import { connect } from "./helpers";
+import { Messages } from "./interfaces";
 
-interface Messages extends RowDataPacket {
-    id: number,
-    from_user: number,
-    text: string,
-    to_user: number,
-    sent_date: Date,
-    seen: number
-}
-
-export function newMessageCountController (req:any, res:any) {
+export async function newMessageCountController (req:any, res:any) {
+    const conn = await connect();
     const userId = req.params.userId;
-    conn.query<Messages[]>('SELECT * from messages WHERE seen = ? AND to_user = ? ORDER BY sent_date DESC',
-    ['0', userId], function (err, results) {
-        if(err) return res.status(500).send(err);
+    try {
+        const [results] = await conn.query<Messages[]>
+        ('SELECT * from messages WHERE seen = ? AND to_user = ? ORDER BY sent_date DESC',['0', userId]);
         return res.status(200).send(results);
-    });
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
 
-export function allMessageController (req:any, res:any) {
-    const userId = req.params.userId;
-    conn.query<Messages[]>('SELECT * from messages WHERE to_user = ? ORDER BY sent_date DESC',
-    [userId], function (err, results) {
-        if(err) return res.status(500).send(err);
-        return res.status(200).send(results);
-    });
+export async function lastMessageController (req:any, res:any) {
+    const conn = await connect();
+    const userId:number = req.params.userId;
+    try {
+        const [results] = await conn.query<Messages[]>(`
+            WITH RankedMessages AS (
+            SELECT
+                id,
+                from_user,
+                to_user,
+                sent_date,
+                text,
+                seen,
+                db_room,
+                ROW_NUMBER() OVER (PARTITION BY from_user ORDER BY sent_date DESC) AS RowNum
+            FROM
+                messages
+                WHERE to_user = ? OR from_user = ?
+                ORDER BY sent_date DESC
+            )
+            SELECT
+            id,
+            from_user,
+            to_user,
+            sent_date,
+            TEXT,
+            seen,
+            db_room
+            FROM
+            RankedMessages
+            WHERE
+            RowNum = 1`,
+        [userId, userId]);
+
+        function filterResults () {
+            let db_room_id: string[] = [];
+            let filteredResults: Messages[] = [];
+            for (let i = 0; i < results.length; i++) {
+                if (!db_room_id.includes(results[i].db_room)) {
+                    db_room_id.push(results[i].db_room);
+                    db_room_id.push(results[i].db_room.split('').reverse().join(''));
+                    filteredResults.push(results[i]);
+                }
+            }
+            return filteredResults;
+        }
+
+        return res.status(200).send(filterResults());
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
 
-export function allChatMessageController (req:any, res:any) {
+export async function allChatMessageController (req:any, res:any) {
+    const conn = await connect();
     const userId = req.params.userId;
     const otherUserId = req.params.otherUserId;
-    conn.query<Messages[]>('SELECT * from messages WHERE (to_user = ? AND from_user = ?) OR (to_user = ? AND from_user = ?) ORDER BY sent_date DESC',
-    [userId, otherUserId, otherUserId, userId], function (err, results) {
-        if(err) return res.status(500).send(err);
+    try {
+        const [results] = await conn.query<Messages[]>('SELECT * from messages WHERE (to_user = ? AND from_user = ?) OR (to_user = ? AND from_user = ?) ORDER BY sent_date DESC',
+        [userId, otherUserId, otherUserId, userId]);
         return res.status(200).send(results);
-    });
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
 
-export function seenChatController (req:any, res:any) {
+export async function seenChatController (req:any, res:any) {
+    const conn = await connect();
     const chats:number[] = req.body.chats;
     const chat_id: string = chats.join(",");
-    conn.query<Messages[]>('UPDATE messages SET seen = 0 WHERE id IN ('+chat_id+')',
-    [], function (err, results) {
-        if(err) return res.status(500).send(err);
-        return res.status(200).send(results);
-    });
+    try {
+        const [result] = await conn.query<Messages[]>('UPDATE messages SET seen = 0 WHERE id IN ('+chat_id+')',[]);
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
+export async function allChattedUsers (req:any, res:any) {
+    const conn = await connect();
+    const userId:number = req.params.userId;
+    try {
+        const [result] = await conn.query<any[]>(`
+        SELECT DISTINCT users.id AS USER_ID, users.first_name, users.last_name
+        FROM messages LEFT JOIN users ON messages.from_user = users.id
+        WHERE (messages.from_user = ? OR messages.to_user = ?) AND users.id != ?`,
+        [userId, userId, userId]);
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 
 }
